72d3a84f43a802550787df3dfa166ed700682f13
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-23 01:45:52 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,17 @@
 # Using Puppet, install flask from pip3
+# Ensure Python 3 is installed
 package { 'python3':
   ensure => present,
+}
+
+exec { 'flask_installed':
+  command => 'pip3 list | grep -q Flask',
+  path    => '/usr/bin',
+  require => Package['python3'],
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  path     => '/bin',
-  require  => Package['python3'],
+  require  => Exec['flask_installed'],
 }
