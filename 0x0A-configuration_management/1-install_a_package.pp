97bede3a456ef2c072a2cd67e48aea916a530c07
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-23 02:50:16 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,6 +9,6 @@ package { 'python3':
 
 exec { 'install_flask':
   require => [Package['python3-pip'], Package['python3']],
-  command => '/usr/bin/pip3 install -q flask==2.1.0',
-  unless  => '/usr/bin/pip3 list | grep -q Flask==2.1.0',
+  command => '/usr/bin/pip3 install -q flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 list | grep -qE "Flask==2.1.0\\s+Werkzeug==2.1.1',
   }
