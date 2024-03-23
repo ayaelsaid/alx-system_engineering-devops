432a359103f2ad045ced0a60d051c211f22bc7fc
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-23 05:46:23 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# connect to a server without typing a password.
+
 file { '/etc/ssh/100-puppet_ssh_config':
   ensure  => file,
   content => 'Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n',
