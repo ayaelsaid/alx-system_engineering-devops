999b284780c239c34c5cee5d8dd55763c6733b2c
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-23 06:16:08 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,13 +4,13 @@
 file_line { 'Turn off passwd auth':
   ensure  => present,
   line    => 'PasswordAuthentication no',
-  path    => '/etc/ssh/100-puppet_ssh_config',
+  path    => '/etc/ssh/ssh_config',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
   line    => 'IdentityFile ~/.ssh/school',
-  path    => '/etc/ssh/100-puppet_ssh_config',
+  path    => '/etc/ssh/ssh_config',
   replace => true,
 }
