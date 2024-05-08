4936fa9aa73896220f5c1572decd82b95afb18f6
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-08 04:57:42 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -20,5 +20,7 @@ def top_ten(subreddit):
             for post in data['data']['children']:
                 my_title = post['data']['title']
                 print(my_title)
+        else
+            print ("None")
     except requests.exceptions.RequestException as e:
         print("None")
