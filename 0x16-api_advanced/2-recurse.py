103efb2cfa308b432a52745d7f306d8c840c962d
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-08 04:54:50 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 
 """
-recursive function that queries"""
+recursive function that queries
+"""
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-  
-  url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-  headers = {'User-Agent':'u/thisisinsider'}
-  try:
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    data = response.json()
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'u/thisisinsider'}
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
 
-    if 'data' in data and 'children' in data['data']:
-      for post in data["data"]["children"]:
-        hot_list.append(post["data"]["title"])
-        after = data['data'].get('after')
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                hot_list.append(post['data']['title'])
+
+            after = data['data'].get('after')
             if after:
                 recurse(subreddit, hot_list, after)
             else:
-              return hot_list
-    else:
-      return None
-
-  except requests.exceptions.RequestException as e:
-    return None
+                return hot_list
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        return None  # Return None if an error occurs during the request
