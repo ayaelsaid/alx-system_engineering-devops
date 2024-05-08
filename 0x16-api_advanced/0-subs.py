6a167dbf7e3d3395d899615bad11d5a6aad523b4
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-08 04:17:28 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ function that queries the Reddit API and returns the number of subscribers
 """
 
 import requests
-import json
+
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
