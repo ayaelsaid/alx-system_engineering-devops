1cf3b6fa115e8d1cab8903aa4fae7f82d851e33a
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-08 04:37:15 +0300

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+"""
+queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'u/thisisinsider'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                my_title = post['data']['title']
+                print(my_title)
+    except requests.exceptions.RequestException as e:
+        print("None")
